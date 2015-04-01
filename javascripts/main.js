1d7c9fc1c6b13d0a1955c906a7e538992ea2aa5d
Lin Lin <lynnlin1988@gmail.com>
2015-04-01 13:21:59 -0400

    editted social media icons

--- javascripts/main.js
+++ javascripts/main.js
@@ -1,1 +1,33 @@
-console.log('This would be the main JS file.');
+
+$(document).ready(function(){
+
+
+	//Social media icons when mouseover
+    $("#facebook_icon").mouseover(function(){
+    	$("#facebook_icon").fadeTo(0, 1);
+    });
+    $("#facebook_icon").mouseout(function(){
+    	$("#facebook_icon").fadeTo(0, 0.4);
+    });
+
+    $("#twitter_icon").mouseover(function(){
+    	$("#twitter_icon").fadeTo(0, 1);
+    });
+    $("#twitter_icon").mouseout(function(){
+    	$("#twitter_icon").fadeTo(0, 0.4);
+    });
+
+    $("#linkedin_icon").mouseover(function(){
+    	$("#linkedin_icon").fadeTo(0, 1);
+    });
+    $("#linkedin_icon").mouseout(function(){
+    	$("#linkedin_icon").fadeTo(0, 0.4);
+    });
+
+    $("#youtube_icon").mouseover(function(){
+    	$("#youtube_icon").fadeTo(0, 1);
+    });
+    $("#youtube_icon").mouseout(function(){
+    	$("#youtube_icon").fadeTo(0, 0.4);
+    });
+});
